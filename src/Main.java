3aa376fa9760368a02ed4c59d673afde4ffe0602
Jon Mockbee <mockbee1000@gmail.com>
2019-07-17 10:01:17 -0400

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int calcFeetAndInchesToCentimeters (int feet ,int inches)
+    {
+        
+    }
+}
